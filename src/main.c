7cc6c76d1bc4b6b9a586b188cff839f8075eb400
Felipe Diniello <felipediniello@pm.me>
2023-09-11 17:16:02 -0300

    Add comments for each parameter

--- src/main.c
+++ src/main.c
@@ -11,27 +11,40 @@ int main(int argc, char *argv[]) {
   double CI;   // Lift Coefficient [no unit]
   double rho;  // Air Density [kg/m^3]
   double Aw;   // Wing Area [m^2]
-  double Vw_squared;   // Relative Wind's velocity [m/s]
+  double Vw;   // Relative Wind's velocity [m/s]
   double delta_pressure; // relative wind's pressure [N/m^2]
 
+  // Read samples for Lift Coeffficient, randomly generated:
+  // Normal distribution:  mean = 0.5, std_dev = 0.01 
   CI = read_samples("samples-normal-CI.csv");
   printf("Lift Coefficient = %lf [1] \n", CI);
 
+
+  // Read samples for Air density, randomly generated:
+  // Normal distribution:  mean = 1.3, std_dev = 0.1
   rho = read_samples("samples-normal-rho.csv");
   printf("rho = %lf [Kg/m^3] \n", rho);
 
+
+  // Read samples for Wing Area, randomly generated:
+  // Normal distribution:  mean = 20, std_dev = 0.01
   Aw = read_samples("samples-normal-Wing-Area.csv");
   printf("Wing Area = %lf [m^2] \n", Aw);
 
+
+  // Pressure measured by Pitot Tube 
+  // Uniform distribution:  mean = 24000, +/- 10%
   delta_pressure = read_samples("samples-uniform-delta-pressure.csv");
   printf("delta_pressure = %lf [N/m^2] \n", delta_pressure);
 
 
-  
-  Vw_squared = 2 * delta_pressure / rho;
-  printf("Measured Wind Speed = %lf [m/s]\n", sqrt(Vw_squared));
+  // Calculated wind speed from measured delta in pressure:
+  Vw = sqrt(2 * delta_pressure / rho);
+  printf("Measured Wind Speed = %lf [m/s]\n", Vw);
 
-
+  // Calculated Lift force from 
+  //   * plane's paramters: CI & Aw
+  //   * measured parameters: delta_pressure
   Lift = CI * Aw * delta_pressure;
   printf("Lift Force = %lf [N]\n", Lift);
 
